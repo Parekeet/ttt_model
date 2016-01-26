1bf5cc9341cf4984107f557c0192d7b1cc7e18f8
Patcharin Sakulkoo <psakulkoo@gmail.com>
2016-01-26 12:59:32 -0800

    adding first draft ttt model

--- main.js
+++ main.js
@@ -12,7 +12,26 @@ var board = [
   "", "", "", // 6, 7, 8
 ];
 
+
 //1. START THE GAME
+//      inputs:
+//        - current player, board, won
+//
+//      outputs:
+//        - board is emptied, player is X, won is false
+
+
+var startGame = function() {
+  won = false;
+  currentPlayer = "X";
+  board = [
+  "", "", "", // 0, 1, 2
+  "", "", "", // 3, 4, 5
+  "", "", "", // 6, 7, 8
+  ];
+};
+
+
 //2. MOVE (CHANGES TURN)
 //      inputs:
 //        - whose turn it is,
@@ -37,8 +56,60 @@ var move = function(cellIndex) {
   }
 };
 
+
 //currentPlayer = (currentPlayer === "X" ? "O" : "X"); IS THIS <-- (TERNARY OPERATOR)
 
 
 //3. WIN THE GAME
-var gameWon = function() {};
+//    inputs:
+//      - board
+
+//    outputs:
+//      - true or false
+
+
+var gameWon = function() {
+  if (
+    ((board[0] === board[1]) && (board[0] === board[2]) && board[0] !== "") ||
+    ((board[3] === board[4]) && (board[3] === board[5]) && board[3] !== "") ||
+    ((board[6] === board[7]) && (board[6] === board[8]) && board[6] !== "")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
